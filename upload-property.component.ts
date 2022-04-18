eedd38461caf66f5ec830d086c525018391e0dd3
revadeshpande <66058637+revadeshpande@users.noreply.github.com>
2022-04-18 13:55:30 +0530

    Add files via upload

---
+++ upload-property.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-property',
+  templateUrl: './upload-property.component.html',
+  styleUrls: ['./upload-property.component.css']
+})
+export class UploadPropertyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
